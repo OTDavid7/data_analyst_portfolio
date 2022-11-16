e6fa03d3b820e771544afeab4096145812c92cce
OTDavid7 <92827464+OTDavid7@users.noreply.github.com>
2022-11-15 16:49:53 -0800

    iindex

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function resume() {
                     alert("You are about to download my resume!");
-                    alert("I am looking forward chatting with you")
+                    alert("I am looking forward to hearing with you")
                     }
